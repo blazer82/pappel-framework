dd5824d9b4d74d711e9831e03690be3f6aa77260
Unknown <staebler@deluxx.ch>
2017-12-13 23:50:18 +0100

    Added some more wrapper methods and code doc

--- src/main/kotlin/framework/express/Application.kt
+++ src/main/kotlin/framework/express/Application.kt
@@ -2,15 +2,39 @@ package framework.express
 
 class Application(external private val app: dynamic) {
 
+    /**
+     * Handle GET request for [path]
+     */
     fun get(path: String, callback: (request: Request, response: Response) -> Unit) {
         app.get(path) {
             req, res -> callback.invoke(Request(req), Response(res))
         }
     }
 
-    fun listen(port: Int, callback: () -> Unit) {
+    /**
+     * Starts listening on UNIX socket with [socketPath]
+     */
+    fun listen(socketPath: String, callback: (() -> Unit)?) {
+        app.listen(socketPath) {
+            callback?.invoke()
+        }
+    }
+
+    /**
+     * Starts listening on [port]
+     */
+    fun listen(port: Int, callback: (() -> Unit)?) {
         app.listen(port) {
-            callback.invoke()
+            callback?.invoke()
+        }
+    }
+
+    /**
+     * Starts listening on [host]:[port]
+     */
+    fun listen(port: Int, host: String, callback: (() -> Unit)?) {
+        app.listen(port, host) {
+            callback?.invoke()
         }
     }
 
--- src/main/kotlin/framework/express/Response.kt
+++ src/main/kotlin/framework/express/Response.kt
@@ -2,14 +2,23 @@ package framework.express
 
 class Response(external private val res: dynamic) {
 
+    /**
+     * Sends String response
+     */
     fun send(string: String) {
         res.send(string)
     }
 
+    /**
+     * Sets response [type]
+     */
     fun type(type: String) {
         res.type(type)
     }
 
+    /**
+     * Sends JSON response
+     */
     fun json(data: Any) {
         res.json(data)
     }
