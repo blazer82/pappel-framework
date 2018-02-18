7cd92d361ac2bfdc6d439a6588625d88fee20e8b
Unknown <staebler@deluxx.ch>
2018-02-18 12:16:54 +0100

    Added react concept tests

--- web/src/main/kotlin/main.kt
+++ web/src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
 import kotlinx.html.*
 import kotlinx.html.js.*
+import react.*
+import react.dom.*
 import kotlin.browser.*
 
 fun main(args: Array<String>) {
@@ -9,4 +11,27 @@ fun main(args: Array<String>) {
     println(element)
 
     element?.innerHTML = "dyn text"
+
+    ReactDOM.render(document.getElementById("react-test")) {
+        Application {}
+    }
+
 }
+
+class Application : ReactDOMComponent<ReactComponentNoProps, ApplicationPageState>() {
+
+    init {
+        state = ApplicationPageState()
+    }
+
+    override fun ReactDOMBuilder.render() {
+        p {
+            +"react test"
+        }
+    }
+
+    companion object : ReactComponentSpec<Application, ReactComponentNoProps, ApplicationPageState>
+
+}
+
+class ApplicationPageState : RState
